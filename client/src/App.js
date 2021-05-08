f3bc5ec112f18d9e1d995423f35540d9d2ce8238
Danny Vo <dmvo@ucsd.edu>
2021-05-08 01:55:10 -0700

    Initial header component + issues
    react-schedule-selector has a ton of issues regarding how it uses
    unspecified types and some other errors that I don't think is a good
    idea to get into. Here's a basic header, but we'll probably need to
    implement our own version of the scheduler :/

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Home from './js/home/user_home.js';
+import Header from './components/Header'
 
 function App() {
   return (
     <>
+      <header>
+        <Header />
+      </header>
       <Router>
         <Switch>
           <Route path='/home' exact component={Home} />
